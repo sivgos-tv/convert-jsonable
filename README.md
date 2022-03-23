262d8f27503a7a67ffd518d74c2d7541e473ae2c
Sivaranjan Goswami <74286730+sivgos-tv@users.noreply.github.com>
2022-03-23 13:18:05 +0530

    added code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # convert-jsonable
+
+Converts a dictionary or list of dictionary to Jsonable format (if not already jsonable).
+
---
+++ convert_jsonable.py
@@ -0,0 +1,30 @@
+import json
+from typing import List, Dict, Union
+
+def convert_to_jsonable_type(x):
+    try:
+        return int(x)
+    except:
+        try:
+            return float(x)
+        except:
+            return str(x)
+        
+def convert_jsonable(data: Union[Dict, List[Dict]]):
+    if isinstance(data, list):
+        for i in range(len(data)):
+            if not (isinstance(data[i], dict) or isinstance(data[i], list)):
+                raise 'Only supported list items : dict'
+            data[i] = convert_jsonable(data)
+    for key in data.keys():
+        if isinstance(data[key], dict):
+            data[key] = convert_jsonable(data[key])
+        elif isinstance(data[key], list):
+            for i in range(len(data[key])):
+                if isinstance(data[key][i], dict) or sinstance(data[key][i], list):
+                    data[key][i] = convert_jsonable(data[key][i])
+                else:
+                    data[key][i] = convert_to_jsonable_type(data[key][i])
+        else:
+            data[key] = convert_to_jsonable_type(data[key])
+    return data
